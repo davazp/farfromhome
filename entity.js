14f88e65c4c532deb3d5cf32dabfe100fcb2bcb3
David Vázquez Púa <davazp@gmail.com>
2019-01-25 19:17:12 +0100

    Populate the universe

--- entity.js
+++ entity.js
@@ -1,5 +1,7 @@
 const C = 299792458; /* m/s */
 
+const Λ = 10 * C;
+
 function distance(p1, p2) {
   return Math.sqrt(
     (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2 + (p1[2] - p2[2]) ** 2
@@ -11,32 +13,59 @@ class Entity {
     this.pos = [x, y, z];
   }
   receivedMessage(origin, message) {
-    console.log({ origin, message });
+    console.log({ pos: this.pos, origin, message });
+  }
+
+  sendMessage(source, message) {
+    const d = distance(this.pos, source.pos);
+    const SECOND = 1000;
+    const delay = (d / C) * SECOND;
+    setTimeout(() => {
+      this.receivedMessage(source, message);
+    }, delay);
+  }
+}
+
+class Player extends Entity {}
+
+class Planet extends Entity {
+  receivedMessage(origin, message) {
+    super.receivedMessage(origin, message);
+    switch (message.type) {
+      case "ping":
+        origin.sendMessage(this, { type: "pong" });
+        return;
+    }
   }
 }
 
 class Universe {
   constructor() {
-    this.home = {
-      x: 0,
-      y: 100000000000000,
-      z: 0
-    };
-    this.player = [0, 0, 0];
+    this.home = new Entity([0, 10 * C, 0]);
+
+    this.entities = [];
+    for (let i = 0; i < 3; i++) {
+      this.entities.push(
+        new Planet(
+          Λ * (2 * Math.random() - 1),
+          Λ * (2 * Math.random() - 1),
+          Λ * (2 * Math.random() - 1)
+        )
+      );
+    }
+
+    this.player = new Player(0, 0, 0);
   }
 
-  sendMessage(target, message) {
-    const d = distance(target.pos, this.player);
-    const SECOND = 1000;
-    const delay = (d / C) * SECOND;
-    console.log({ d, delay });
-    setTimeout(() => {
-      target.receivedMessage(this.player, message);
-    }, delay);
+  broadcast(message) {
+    this.entities.forEach(e => {
+      if (true || distance(e.pos, this.player.pos) < 5 * C) {
+        e.sendMessage(this.player, { type: "ping" });
+      }
+    });
   }
 }
 
 const U = new Universe();
-const planet = new Entity(299792458, 0, 0);
 
-U.sendMessage(planet, "hello world!");
+U.broadcast("hello world!");
