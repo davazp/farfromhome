50c8f69c443f6cb4e52c1048213042e17343c42d
David Vázquez Púa <davazp@gmail.com>
2019-01-26 01:21:49 +0100

    Communicate entity positions to the client

--- client/src/index.js
+++ client/src/index.js
@@ -1,27 +1,90 @@
-import { Universe } from "./entity";
-import { View } from "./view";
-
 import io from "socket.io-client";
-
-const universe = new Universe();
-const view = new View(universe);
+import * as THREE from "three";
+import "./enableThree";
+import "three/examples/js/controls/OrbitControls";
 
 const socket = io("http://localhost:3000/");
 
-socket.on("connect", function() {
+socket.on("connect", () => {
   console.log("connected");
 });
-socket.on("event", function(data) {
-  console.log("event", { data });
-});
-socket.on("disconnect", function() {
+
+socket.on("disconnect", () => {
   console.log("disconnect");
 });
 
-view.start();
-view.update();
+class View {
+  constructor() {
+    this.radar = new Map();
+    socket.on("message", message => {
+      this.updateEntity(message.from, message.position);
+    });
+  }
 
-setInterval(() => {
-  universe.tick(0.01);
-  view.update();
-}, 10);
+  getOrMakeEntity(id) {
+    let obj = this.radar.get(id);
+    if (!obj) {
+      const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
+      const material = new THREE.MeshBasicMaterial({
+        color: new THREE.Color("red")
+      });
+      obj = new THREE.Mesh(geometry, material);
+      this.scene.add(obj);
+      this.radar.set(id, obj);
+    }
+    return obj;
+  }
+
+  updateEntity(id, position) {
+    const obj = this.getOrMakeEntity(id);
+    const [x, y, z] = position;
+    obj.position.x = x;
+    obj.position.y = y;
+    obj.position.z = z;
+    return obj;
+  }
+
+  start() {
+    let scene = new THREE.Scene();
+    this.scene = scene;
+    let camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+
+    let renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    let controls = new THREE.OrbitControls(camera);
+
+    camera.position.x = 0;
+    camera.position.y = 0;
+    camera.position.z = 5;
+
+    const radius = 5;
+    const segments = 30;
+    const rings = 30;
+
+    const geometry = new THREE.SphereGeometry(radius, segments, rings);
+    const material = new THREE.MeshBasicMaterial({
+      color: 0x303030,
+      wireframe: true
+    });
+
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    function animate() {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+      controls.update();
+    }
+    animate();
+  }
+}
+
+const view = new View();
+view.start();
--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,146 @@
 const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+const uuidv1 = require("uuid/v1");
 
-app.get("/", function(_req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
+const C = 1;
+const Λ = 10 * C;
+
+function distance(p1, p2) {
+  return Math.sqrt(
+    (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2 + (p1[2] - p2[2]) ** 2
+  );
+}
+
+class Entity {
+  constructor(x, y, z) {
+    this.id = uuidv1();
+    this.pos = [x, y, z];
+    this.messageQueue = [];
+  }
+
+  update(_dt) {}
+
+  tick(dt) {
+    this.update(dt);
+    const messages = this.messageQueue;
+    this.messageQueue = [];
+    messages.forEach(m => {
+      m.reach += dt * C;
+      const d = distance(this.pos, m.sourcePosition);
+      if (d <= m.reach) {
+        this.receivedMessage(m.source, m.message);
+      } else {
+        this.messageQueue.push(m);
+      }
+    });
+  }
+
+  receivedMessage(origin, message) {
+    console.log({ type: "received", origin, message });
+  }
+
+  sendMessage(source, message) {
+    this.messageQueue.push({
+      reach: 0,
+      sourcePosition: source.pos,
+      source,
+      message
+    });
+  }
+}
+
+class Planet extends Entity {
+  receivedMessage(origin, message) {
+    super.receivedMessage(origin, message);
+    switch (message.type) {
+      case "ping":
+        origin.sendMessage(this, { type: "pong" });
+        return;
+    }
+  }
+}
+
+class Player extends Entity {
+  receivedMessage(origin, message) {
+    io.emit("message", { ...message, from: origin.id });
+  }
+}
+
+class Spaceship extends Entity {
+  constructor(x, y, z, owner) {
+    super(x, y, z);
+    this.updateVelocity([0, 0, 0]);
+    setInterval(() => {
+      owner.sendMessage(this, { type: "heartbeat", position: this.pos });
+    }, 1000);
+  }
+  updateVelocity(newvel) {
+    this.velocity = newvel;
+  }
+  update(dt) {
+    const [x, y, z] = this.pos;
+    const [vx, vy, vz] = this.velocity;
+    this.pos = [x + dt * vx, y + dt * vy, z + dt * vz];
+  }
+}
+
+class Universe {
+  constructor() {
+    this.player = new Player(0, 0, 0);
+
+    this.entities = [];
+
+    for (let i = 0; i < 1000; i++) {
+      this.entities.push(
+        new Planet(
+          Λ * (2 * Math.random() - 1),
+          Λ * (2 * Math.random() - 1),
+          Λ * (2 * Math.random() - 1)
+        )
+      );
+    }
+
+    for (let i = 0; i < 100; i++) {
+      const s = new Spaceship(
+        Λ * (2 * Math.random() - 1),
+        Λ * (2 * Math.random() - 1),
+        Λ * (2 * Math.random() - 1),
+        this.player
+      );
+
+      s.updateVelocity([
+        (C * Math.random()) / 10,
+        (C * Math.random()) / 10,
+        (C * Math.random()) / 10
+      ]);
+
+      this.entities.push(s);
+    }
+  }
+
+  tick(dt) {
+    this.player.tick(dt);
+    this.entities.forEach(e => e.tick(dt));
+  }
+
+  broadcast(message) {
+    this.entities.forEach(e => {
+      e.sendMessage(this.player, message);
+    });
+  }
+}
+
+const universe = new Universe();
+
+setInterval(() => {
+  universe.tick(1);
+}, 1000);
 
 io.on("connection", function(socket) {
-  console.log("a user connected");
+  socket.on("message", _message => {
+    socket.emit("message", { type: "pong" });
+  });
 });
 
 http.listen(3000, function() {
